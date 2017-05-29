38a81582636a16702ba105a857c6a0da755c59bf
ivan <ivan.migov@gmail.com>
2017-05-29 20:16:42 +0300

    loash

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log('starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes.js');
+
+// console.log(_.isString(true));
+// console.log(_.isString('string'));
+
+
+// let res = notes.addNote();
+// let sum = notes.add(21,-5);
+// console.log(sum);
+
+// var user = os.userInfo();
+// fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ notes.js
@@ -0,0 +1,12 @@
+console.log('starting nodes.js');
+
+module.exports.addNote = ()=> {
+  console.log('addNote');
+  return 'New note';
+};
+module.exports.add = (a,b)=> {
+  return a+b;
+};
+
+
+
